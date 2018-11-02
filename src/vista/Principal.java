2baacf76d231661c9e32637aff322e14b2e11f98
Caleb Castañeda <Caleb Castaeda@SAMSUNG-LAPTOP>
2018-11-02 16:21:50 -0600

    LISTO AHORA SI

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -1,21 +1,14 @@
 package vista;
 
-
-
-
 import javax.swing.JOptionPane;
 
 import controlador.ArrayObjetos;
 import modelo.*;
-
-
 public class Principal {
 
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Bienvenido al sistema bancario de PIAL.SA");
 		ArrayObjetos objeto = new ArrayObjetos();
 
-		
-		
 	}
 }
